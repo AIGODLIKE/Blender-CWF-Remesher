db88e0a2bf69cbf9ec14d3c48dee53750b56cbee
KarryCharon <karrycharon@qq.com>
2024-06-05 23:53:20 +0800

    support bl3.5 3.6

--- src/cwf/props.py
+++ src/cwf/props.py
@@ -1,19 +1,23 @@
 import bpy
 from ..i18n.ctx import PCTX
 
+KWARGS = {"translation_context": PCTX}
+if bpy.app.version < (4, 0):
+    KWARGS.pop("translation_context")
+
 
 class Props(bpy.types.PropertyGroup):
-    xyz_path: bpy.props.StringProperty(name="XYZ Path", subtype="FILE_PATH", translation_context=PCTX)
-    advanced: bpy.props.BoolProperty(default=False, name="Advanced", translation_context=PCTX)
-    p_samples: bpy.props.IntProperty(name="Samples", default=1000, min=10, max=100000, translation_context=PCTX)
-    p_fnum: bpy.props.IntProperty(name="Fnum", default=4, min=4, max=100, translation_context=PCTX)
-    p_alpha: bpy.props.FloatProperty(name="Alpha", default=1.0, min=0.01, max=1, translation_context=PCTX)
-    p_eplison: bpy.props.FloatProperty(name="Eplison", default=1.0, min=0.0001, max=1, translation_context=PCTX)
-    p_lambda: bpy.props.FloatProperty(name="Lambda", default=1.0, min=0.0001, max=1, translation_context=PCTX)
-    p_decay: bpy.props.FloatProperty(name="Decay", default=0.95, min=0.01, max=1, translation_context=PCTX)
+    xyz_path: bpy.props.StringProperty(name="XYZ Path", subtype="FILE_PATH", **KWARGS)
+    advanced: bpy.props.BoolProperty(default=False, name="Advanced", **KWARGS)
+    p_samples: bpy.props.IntProperty(name="Samples", default=1000, min=10, max=100000, **KWARGS)
+    p_fnum: bpy.props.IntProperty(name="Fnum", default=4, min=4, max=100, **KWARGS)
+    p_alpha: bpy.props.FloatProperty(name="Alpha", default=1.0, min=0.01, max=1, **KWARGS)
+    p_eplison: bpy.props.FloatProperty(name="Eplison", default=1.0, min=0.0001, max=1, **KWARGS)
+    p_lambda: bpy.props.FloatProperty(name="Lambda", default=1.0, min=0.0001, max=1, **KWARGS)
+    p_decay: bpy.props.FloatProperty(name="Decay", default=0.95, min=0.01, max=1, **KWARGS)
 
-    pre_simplify: bpy.props.BoolProperty(default=True, name="Pre Simplify", translation_context=PCTX)
-    pre_simplify_ratio: bpy.props.FloatProperty(default=.3, min=0.01, max=1, name="Pre Simplify Ratio", translation_context=PCTX)
+    pre_simplify: bpy.props.BoolProperty(default=True, name="Pre Simplify", **KWARGS)
+    pre_simplify_ratio: bpy.props.FloatProperty(default=.3, min=0.01, max=1, name="Pre Simplify Ratio", **KWARGS)
 
     def set_params(self, params):
         """
