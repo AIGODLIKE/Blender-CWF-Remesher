29682897f89379dd09976f3ce7886dbd44aeb32a
KarryCharon <karrycharon@qq.com>
2024-06-06 10:00:24 +0800

    move macos to macosx

--- cxxlibs/__init__.py
+++ cxxlibs/__init__.py
@@ -1,6 +1,6 @@
 import platform
 if platform.system() == "Darwin":
-    from .macos import remesh
+    from .macosx import remesh
 elif platform.system() == "Windows":
     from .windows import remesh
 else:
